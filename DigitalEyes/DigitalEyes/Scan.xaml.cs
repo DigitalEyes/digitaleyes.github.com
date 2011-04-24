a0e099d22fab3c64a551aeb0df00d358ef1a68fd
unknown <Leah@.(none)>
2011-04-24 17:58:21 -0500

    added hardcoding.  still have to do directions

--- DigitalEyes/DigitalEyes/Scan.xaml.cs
+++ DigitalEyes/DigitalEyes/Scan.xaml.cs
@@ -26,7 +26,7 @@ namespace DigitalEyes
             InitializeComponent();
             /*This is the hardcoded sample result of a tag scan. The tag will return a string which will be referenced 
              * with a location in our data structure and will return information about the location*/
-            textBlock1.Text = "This is room 3437. Multiple Computer Science and Chemical Engineering classes meet here.  It is a traditional classroom setup with chairs attached to the desks.  There are no stairs inside this room.";
+            //textBlock1.Text = "This is room 3437. Multiple Computer Science and Chemical Engineering classes meet here.  It is a traditional classroom setup with chairs attached to the desks.  There are no stairs inside this room.";
         }
         /*Load and apply the saved values for the font size, background color, and font color */
         private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
@@ -51,24 +51,24 @@ namespace DigitalEyes
                 {
                     double mediumFontSize = Convert.ToDouble(FontSizeObject);
                     button2.FontSize = mediumFontSize;
-                    button1.FontSize = mediumFontSize;
+                    //button1.FontSize = mediumFontSize;
                     
 
                     /*Ensure that buttons resize appropriately, changes the height of button along with the fontSize
                      * so that the font will fit inside the button box*/
                     if (mediumFontSize < 23)
                     {
-                        button1.Height = mediumFontSize * 4;
+                        //button1.Height = mediumFontSize * 4;
                         button2.Height = mediumFontSize * 4;
                     }
                     else if (mediumFontSize < 30)
                     {
-                        button1.Height = mediumFontSize * 3;
+                        //button1.Height = mediumFontSize * 3;
                         button2.Height = mediumFontSize * 3;
                     }
                     else
                     {
-                        button1.Height = mediumFontSize * 2.5;
+                        //button1.Height = mediumFontSize * 2.5;
                         button2.Height = mediumFontSize * 2.5;
                     }
                 }
@@ -80,7 +80,7 @@ namespace DigitalEyes
                 {
                     double smallFontSize = Convert.ToDouble(FontSizeObject);
                     ApplicationTitle.FontSize = smallFontSize;
-                    textBlock1.FontSize = smallFontSize;
+                    //textBlock1.FontSize = smallFontSize;
                 }
             }
             /*Set the background color of the page from the saved color*/
@@ -101,9 +101,9 @@ namespace DigitalEyes
                     ApplicationTitle.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
                     PageTitle.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
                     ApplicationTitle.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
-                    button1.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
+                    //button1.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
                     button2.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
-                    textBlock1.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
+                    //textBlock1.Foreground = new SolidColorBrush(GetColorFromHex(col).Color);
                 }
             }
         }
@@ -131,6 +131,19 @@ namespace DigitalEyes
         private void button2_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void SEC3437_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/SEC3437.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void WomensBathroom_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/WomensBathroom.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void Elevator_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Elevator.xaml", UriKind.RelativeOrAbsolute));
         }
     }
 }
---
+++ DigitalEyes/DigitalEyes/WomensBathroom.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace DigitalEyes
+{
+    public partial class WomensBathroom : PhoneApplicationPage
+    {
+        public WomensBathroom()
+        {
+            InitializeComponent();
+        }
+
+        private void Directions_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Directions.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
